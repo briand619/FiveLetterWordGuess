b02ba8421c0672120f864578e0f5928fffa608e3
Brian Dobberteen <dobberteen@hotmail.com>
2024-03-08 15:47:36 -0600

    Relocated POCO classes. Fixed warnings, mostly related to nullables.

---
+++ Models/LetterStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace FiveLetterWordGuess.Models
+{
+    public enum LetterStatus
+    {
+        NotChecked,
+        NotInWord,
+        InWordWrongPlace,
+        InWordRightPlace,
+        NonWord,
+    }
+
+}
