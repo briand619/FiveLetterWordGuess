cd568fde9abd9d6dfb8b4c01ee6f1ce4d6f26647
Brian Dobberteen <dobberteen@hotmail.com>
2024-03-08 15:47:36 -0600

    Relocated POCO classes. Fixed warnings, mostly related to nullables.

--- GameStatistics.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FiveLetterWordGuess;
-public class GameStatistics
-{
-	public int TotalGames { get; set; }
-	public int Wins { get; set; }
-	public int Losses => TotalGames - Wins;
-	public int CurrentStreak { get; set; }
-	public int[] GuessesUsed { get; set; }
-}
